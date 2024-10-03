5ed267470c2e753649d57a5ab9292c6463027795
Beyza Nur Tekerek <beyzanurtekerk@gmail.com>
2024-10-04 00:26:46 +0300

    Initial Commit

---
+++ CircularShapeSwiftUI/CircularShapeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircularShapeSwiftUIApp.swift
+//  CircularShapeSwiftUI
+//
+//  Created by Beyza Nur Tekerek on 4.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct CircularShapeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
